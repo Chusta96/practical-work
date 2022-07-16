0c2690c1aabb8d8d8593ea65b0f124244a934a3f
Chusta <i.chusta18@mail.ru>
2022-07-16 11:43:40 +0300

    программа определяющие самое большое число из 3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите число a ");
+Console.WriteLine("введите число b ");
+Console.WriteLine("введите число c ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+int b = Convert.ToInt32(Console.ReadLine( ));
+int c = Convert.ToInt32(Console.ReadLine( ));
+
+int max = a; 
+
+if (a > max) Console.WriteLine(max = a);
+
+if (b > max) Console.WriteLine(max = b);
+
+if (c > max) Console.WriteLine(max = c);
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
