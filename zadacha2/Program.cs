74a5482abe1b31c8038cd78f9e8e93e199b91559
Chusta <i.chusta18@mail.ru>
2022-07-16 11:34:38 +0300

    Программа на два числа

---
+++ zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число a ");
+Console.WriteLine("введите число b ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+int b = Convert.ToInt32(Console.ReadLine( ));
+
+int max = a; 
+
+if (a > max) Console.WriteLine(max = a);
+
+if (b > max) Console.WriteLine(max = b);
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
