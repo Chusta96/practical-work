b565d702d22f28b75f7e25aabbddb61486eea45f
Chusta <i.chusta18@mail.ru>
2022-07-16 18:45:08 +0300

    программа на определение четного не четного числа

---
+++ zadach6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+      Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}  
+    
+
+
